23d4c0717cde107f269a5f0c18e2b37612c113d5
Michał Bąk <michalbak000@gmail.com>
2025-02-19 18:07:10 +0100

    added tabs buttons to Tabs component

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,5 +1,14 @@
 export function Tabs() {
+  const tabs = ['All', 'Open', 'Completed']
   return (
-    <nav></nav>
+    <nav className="tab-container">
+      {tabs.map((tab, tabIndex) => {
+        return (
+          <button key={tabIndex} className="tab-button">
+            <h4>{tab} <span>(0)</span></h4>
+          </button>
+        )
+      })}
+    </nav>
   )
 }
