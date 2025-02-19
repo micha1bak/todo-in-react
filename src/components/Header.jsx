c4f90be06cc1436d06d906c77a5228efc07691f5
Michał Bąk <michalbak000@gmail.com>
2025-02-19 18:38:53 +0100

    added taskOrTasks to check if plural

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 export function Header(props) {
   const { todos } = props;
   const todosLength = todos.length;
+
+  const isTasksPlural = todos.length != 1;
+  const taskOrTasks = (isTasksPlural) ? 'tasks' : 'task'
   return (
     <header>
-      <h1 class="text-gradient">You have {todosLength} open tasks</h1>
+      <h1 class="text-gradient">You have {todosLength} open {taskOrTasks}</h1>
     </header>
   )
 }
