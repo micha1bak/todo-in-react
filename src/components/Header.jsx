ce89cc05303866635486549cbd0daff7bb86846b
Michał Bąk <michalbak000@gmail.com>
2025-02-19 18:31:06 +0100

    added todosLength to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
-export function Header() {
+export function Header(props) {
+  const { todos } = props;
+  const todosLength = todos.length;
   return (
     <header>
-      <h1 class="text-gradient">You have # open tasks</h1>
+      <h1 class="text-gradient">You have {todosLength} open tasks</h1>
     </header>
   )
 }
